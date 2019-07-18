9b4ae5095c64323641af699b1e77fdb6d3669a57
anitooni <33741895+anitooni@users.noreply.github.com>
2019-07-18 22:29:10 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -30,3 +30,17 @@ _gaq.push(['_trackPageview']);
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
+
+function includeHTML(divContainer, urlHTML) {
+        let xhttp = new XMLHttpRequest();
+
+        xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            if (this.status == 200) { divContainer.innerHTML = this.responseText; }
+            if (this.status == 404) { divContainer.innerHTML = "Page not found."; }
+        }
+    }
+
+    xhttp.open("GET", urlHTML, true);
+    xhttp.send();
+}
