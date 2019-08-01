7ce20fe2d172a8b630ffd7612bce7632b105d37c
anitooni <33741895+anitooni@users.noreply.github.com>
2019-08-01 11:48:24 +0900

    Add files via upload

---
+++ MenuLoad_kr.js
@@ -0,0 +1,18 @@
+function loadMenu(){
+  document.write(
+  "  <nav>",
+  "    <div class='toggle'><span>Menu</span></div>",
+  "    <div class='menu-container'>",
+  "    <ul class='menu visually-hidden'>",
+  "      <li><a href='./index_kr.html'>HOME</a></li>",
+  "      <li><a href='./About_kr.html'>ABOUT</a></li>",
+  "      <li><a href='./Projects_kr.html'>PROJECTS</a></li>",
+  "      <li><a href='./Works_kr.html'>WORKS</a></li>",
+  "      <li><a href='./Blog_kr.html'>BLOG</a></li>",
+  "      <li><a href='./Lecture_kr.html'>LECTURE</a></li>",
+  "      <li><a href='./Contact_kr.html'>CONTACT</a></li>",
+  "    </ul>",
+  "    </div>",
+  "  </nav>"
+  );
+};
